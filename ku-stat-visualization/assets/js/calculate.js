660cbaa67cc38c014c86518a614b9f215ee8e30a
bbgun1996 <tanaporn.tha@ku.th>
2019-05-21 03:23:40 +0700

    test2

--- ku-stat-visualization/assets/js/calculate.js
+++ ku-stat-visualization/assets/js/calculate.js
@@ -1,136 +1,54 @@
-function calNetAddr(ip, subnet) {
-    var result = [];
-    var subnetSet = binToDec(binSubnet(subnet)).split('.');
-    ip = ip.split('.');
-    for (var i = 0; i < 4; i++) {
-        var tmp = Number(ip[i]);
-        result.push(tmp & parseInt(subnetSet[i]));
-    }
-    return result.join('.');
-}
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBudget);
+google.charts.setOnLoadCallback(drawChart);
 
-function binSubnet(subnet) {
-    var bin = '';
-    for (var i = 0; i < 32; i++) {
-        bin += i < subnet ? 1 : 0;
-    }
-    return bin;
-}
+function drawBudget() {
+      var data = google.visualization.arrayToDataTable([
+        ['Year','Budget'],
+        ['2546', 45148], ['2547', 44019], ['2548', 44389], ['2549', 52007],
+        ['2550', 61468], ['2551',65435], ['2552',70900], ['2553',71625], 
+        ['2554',86905], ['2555', 91997], ['2556',99788], ['2557',106103], 
+        ['2558',109658], ['2559',121202], ['2560',126197]
+      ]);
 
-function binToDec(bin) {
-    var result = [];
-    for (var i = 0; i < 4; i++) {
-        result.push(parseInt(bin.slice(i * 8, (i + 1) * 8), 2));
-    }
-    return result.join('.');
-}
-
-function decToBin(dec) {
-    var result = '';
-    dec = dec.split('.');
-    for (i in dec) {
-        var tmp = parseInt(dec[i]).toString(2);
-        while (tmp.length < 8) {
-            tmp = '0' + tmp;
+      var options = {
+        hAxis: {
+          title: 'Year'
+        },
+        vAxis: {
+          title: 'Budget (Million)'
         }
-        result += tmp;
-    }
-    return result;
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      options = {
+        // title: 'My Daily Activities',
+        fontName:'Roboto',
+        height: 400,
+      };
+
+      chart.draw(data, options);
 }
 
-function getWildCard(subnet) {
-    var result = '';
-    for (var i = 0; i < 32; i++) {
-        result += i < subnet ? 0 : 1;
-    }
-    return result;
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['Task', 'จำนวนบุคลากรทางการแพทย์'],
+      ['แพทย์', 23020],
+      ['ทันตแพทย์', 5198],
+      ['เภสัชกร',  8879],
+      ['พยาบาลวิชาชีพ', 109754],
+      ['พยาบาลเทคนิค', 15595]
+    ]);
+    data.sort([{column: 1, desc: true}]);
+    var options = {
+        fontName:['Bai Jamjuree','Roboto'],
+        title: 'My Daily Activities',
+        colors:['#594F4F', '#547980', '#45ADA8', '#9DE0AD', '#E5FCC2']
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
 }
-
-function getUsableIPRange(netAddr, wildCard) {
-    if (parseInt(wildCard, 2) <= 1) {
-        return '-';
-    }
-    var tmp = netAddr.split('.');
-    for (i in tmp) {
-        tmp[i] = parseInt(tmp[i]);
-    }
-    var start = [tmp[0], tmp[1], tmp[2], (tmp[3] + 1)].join('.');
-    for (var i = 0; i < 4; i++) {
-        tmp[i] += parseInt(wildCard.slice(i * 8, (i + 1) * 8), 2);
-    }
-    var end = [tmp[0], tmp[1], tmp[2], (tmp[3] - 1)].join('.');
-    return start + ' - ' + end;
-}
-
-function getBroadCastAddr(netAddr, wildCard) {
-    if (parseInt(wildCard, 2) == 0) {
-        return netAddr;
-    }
-    var tmp = netAddr.split('.');
-    for (i in tmp) {
-        tmp[i] = parseInt(tmp[i]);
-    }
-    for (var i = 0; i < 4; i++) {
-        tmp[i] += parseInt(wildCard.slice(i * 8, (i + 1) * 8), 2);
-    }
-    return tmp.join('.');
-}
-
-function getClass(subnet) {
-    if (subnet < 16) {
-        return 'A'
-    }
-    else if (subnet < 24) {
-        return 'B'
-    }
-    else {
-        return 'C'
-    }
-}
-
-function isPrivate(ip) {
-    ip = ip.split('.');
-    for (i in ip) {
-        ip[i] = parseInt(ip[i]);
-    }
-    return ip[0] == 10 || (ip[0] == 172 && (ip[1] >= 16 && ip[1] <= 31)) || (ip[0] == 192 && ip[1] == 168);
-}
-
-$('form').submit(function (e) { 
-    e.preventDefault();
-    var rawData = $(this).serializeArray();
-    var data = {}
-    for (i in rawData) {
-        data[rawData[i].name] = rawData[i].value;
-    }
-    var ip = data.ip;
-    var subnet = parseInt(data.subnet);
-    var netAddr = calNetAddr(ip, subnet)
-    var wildCard = getWildCard(subnet);
-    var totalIPs = Math.pow(2, (wildCard.match(/1/g) || []).length);
-
-    var result = [];
-    result.push(ip);
-    result.push(netAddr);
-    result.push(getUsableIPRange(netAddr, wildCard));
-    result.push(getBroadCastAddr(netAddr, wildCard));
-    result.push(totalIPs);
-    result.push(totalIPs > 1 ? totalIPs - 2 : 0);
-    result.push(binToDec(binSubnet(subnet)));
-    result.push(binToDec(wildCard));
-    result.push(binSubnet(subnet));
-    result.push(getClass(subnet));
-    result.push('/ ' + subnet);
-    result.push(isPrivate(ip) ? 'Private' : 'Public');
-    result.push(ip + ' / ' + subnet);
-    result.push(decToBin(ip));
-    result.push(parseInt(decToBin(ip), 2));
-    result.push('0x' + parseInt(decToBin(ip), 2).toString(16));
-    for (var i = 0; i < $('table tr').length; i++) {
-        if (result[i] === undefined) {
-            break;
-        }
-        $('table tr:eq(' + i + ')').find('td:eq(1)').text(result[i]);
-    }
-    $('.result-wrapper').removeClass('hide');
-});
--- ku-stat-visualization/assets/js/init.js
+++ ku-stat-visualization/assets/js/init.js
@@ -1,21 +1,7 @@
-var $subnet = $('.input-group.subnet');
-var lastSelectedSubnet = localStorage.getItem('subnet');
-for (var i = 1; i <= 32; i++) {
-    var bin = '';
-    for (var j = 0; j < 32; j++) {
-        bin += j < i ? '1' : '0';
-    }
-    var ip = '';
-    for (var j = 0; j < 4; j++) {
-        var tmp = parseInt(bin.slice(j * 8, (j + 1) * 8), 2);
-        ip += tmp + (j < 3 ? '.' : '');
-    }
-    var $option = $(`
-        <option value="`+ i + `"` + (parseInt(lastSelectedSubnet) === i ? ' selected' : '') + `>` + ip + ' / ' + i + `</option>
-    `);
-    $subnet.find('select').append($option);
-}
+var $option = $('.input-group.option');
+var $detail = $('.detail')
+$detail.find('h1').text($option.find('select').val());;
 
-$subnet.find('select').change(function() {
-    localStorage.setItem('subnet', $(this).val());
+$option.find('select').change(function() {
+    $detail.find('h1').text($(this).val());;
 });
